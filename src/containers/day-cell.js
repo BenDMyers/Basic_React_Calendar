cfb53cd901c1e7b4d5f192a3f333c9eccfaa86ca
Ben Myers <ben.myers@okstate.edu>
2018-06-24 18:08:13 -0500

    DayCell component

---
+++ src/containers/day-cell.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import _ from 'lodash';
+
+import {days, months, months_abbrev, getWeeks, getPreviousMonth, getNextMonth, getToday} from '../CalendarUtils';
+
+export default class DayCell extends Component {
+    render() {
+        let {year, month, day} = this.props;
+        let dayString = day;
+        if(dayString == 1) {
+            dayString = months_abbrev[month] + " " + dayString;
+        }
+        let todayClass = _.isEqual([year, month, day], getToday()) ? "today-in-month" : '';
+        let otherMonthClass = month === getToday()[1] ? '' : 'other-month';
+        let css = `day-in-month ${todayClass} ${otherMonthClass}`
+
+        return (
+            <td className="day-cell" key={`${year}${month}${day}`}>
+                <span className={css}>{dayString}</span>
+            </td>
+        );
+    }
+}
--- src/containers/monthly-calendar.js
+++ src/containers/monthly-calendar.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import _ from 'lodash';
 
 import {days, months, months_abbrev, getWeeks, getPreviousMonth, getNextMonth, getToday} from '../CalendarUtils';
+import DayCell from './day-cell';
 
 export default class MonthlyCalendar extends Component {
     render() {
@@ -22,31 +23,14 @@ export default class MonthlyCalendar extends Component {
                         <i className="arrow arrow-right"></i>
                     </a></td>
                 </tr></thead></table>
-            <table className="table table-bordered" style={{tableLayout: 'fixed', width: '100%', height: '90%'}}>
+                <table className="table table-bordered" style={{tableLayout: 'fixed', width: '100%', height: '90%'}}>
                     <thead className="thead-dark"><tr>{days_th}</tr></thead>
                     <tbody>
                         {weeks.map((week, index) => {
                             return (
                                 <tr key={'w' + index}>
                                     {week.map(day => {
-                                        let dayString = day[2];
-                                        if(dayString == 1) {
-                                            dayString = months_abbrev[day[1]] + " " + dayString;
-                                        }
-                                        let todayClass = _.isEqual(day, getToday()) ? "today-in-month" : '';
-                                        let css = `day-in-month ${todayClass}`
-                                        if(day[1] != this.props.month) {
-                                            return (
-                                                <td className="day-cell table-cell" key={'d' + day[1] + ' ' + day[2]}>
-                                                    <span className="day-in-month other-month">{dayString}</span>
-                                                </td>
-                                            );
-                                        }
-                                        return (
-                                            <td className="day-cell" key={'d' + day[1] + ' ' + day[2]}>
-                                                <span className={css}>{dayString}</span>
-                                            </td>
-                                        );
+                                        return <DayCell year={day[0]} month={day[1]} day={day[2]} key={day.toString()} />;
                                     })}
                                 </tr>
                             );
